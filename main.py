9cb88775ddb4e7ec0c7e3e74cac28c6ad0f6fbb5
fearlesspinata <vesnalay@gmail.com>
2023-03-21 20:21:42 -0700

    updates
    creating class for team object

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bsoup
 from jinja2 import Environment, PackageLoader, select_autoescape
 from datetime import timedelta
 import pprint
 
-teamId = '1375'
-platform = input('What is the platform of the player you\'re searching? (enter ps4 or xboxone): ').lower()
+#teamId = '1375'
+#platform = input('What is the platform of the player you\'re searching? (enter ps4 or xboxone): ').lower()
 env = Environment(
     loader=PackageLoader('nhl21Stats', 'templates'),
     autoescape=select_autoescape(['html', 'xml'])
@@ -61,19 +61,29 @@ def getTeamStats(teamId):
     matchDict['toa'] = convertSeconds(int(teamStats['toa'])) 
     return matchDict
 
-template = env.get_template('table.html')
-outputFromFile = template.render(playerStat ='variables', playerStatHeader='here')
+#template = env.get_template('table.html')
+#outputFromFile = template.render(playerStat ='variables', playerStatHeader='here')
 
-with open('tableTemp.html', 'w') as fh:
-    fh.write(outputFromFile)
-    fh.close()
+#with open('tableTemp.html', 'w') as fh:
+#    fh.write(outputFromFile)
+#    fh.close()
 
+class Team():
+    def __init__(self, team):
+        self.headers = {'referer': 'www.ea.com'}
+        self.team = team
+        self.platform = 'common-gen5'
+        self.apiEndoint = f"https://proclubs.ea.com/api/nhl/clubs/search?clubName={self.team}&platform={self.platform}"
+        self.session = requests.Session()
 
-    
+    def getTeamID(self):
+        response = self.session.get(self.apiEndoint, headers=self.headers)
+        print(response.json())
 
-<<<<<<< HEAD
-#matchInfo = getMatchHistory(teamId, platform)[0]['clubs'][teamId]['toa']
-#pprint.pprint(matchInfo)
-=======
-pprint.pprint(stats[0]['players']['73254']['1716353852'])
->>>>>>> dd21aaa932c38a866af30de6ee1e65e1eea36398
+team = Team('Whitebucks')
+
+s = requests.Session()
+headers = {'referer': 'www.ea.com', 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+           'accept-language': 'en-US,en;q=0.9', 'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36'}
+r = s.get('https://proclubs.ea.com/api/nhl/clubs/search?clubName=Whitebucks&platform=common-gen5', headers=headers)
+print(r.json())
